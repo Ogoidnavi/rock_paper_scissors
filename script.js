01734bf271c870fc7be4d351579dd10e6a28f94d
Ogoidnavi <ogoidnavi13051997@gmail.com>
2022-05-08 16:05:34 +0100

    add playRound function

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
+"use strict";
 //global variables
 const arrayPlay = ["Rock", "Paper", "Scissors"];
+let computerSelection;
+let playerSelection;
 
 //functions
 function computerPlay() //returns a random play between rock, paper or scissors
 {
    return arrayPlay[Math.floor(Math.random()* arrayPlay.length)];
+}
+
+function playRound(computerSelection, playerSelection)
+{
+    computerSelection = computerPlay().toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection === computerSelection)
+    {
+        return ("It's a tie!");
+    }
+    else if(
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors") || 
+        (playerSelection === "scissors" && computerSelection === "rock")
+    )
+    {
+        return ("You lose!");
+    }
+    else if(
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    )
+    {
+        return ("You win!");
+    }
 }
 
 //Main
